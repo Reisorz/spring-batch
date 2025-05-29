9b8ab7a4d38d4e921c4f21898123e0fb2ec269e6
Reisorz <miguelminib@gmail.com>
2025-05-29 09:16:12 +0200

    Initial commit

---
+++ src/test/java/com/mls/spring_batch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mls.spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
